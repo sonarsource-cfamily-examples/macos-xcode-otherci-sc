29b80bcfba48b9a1bcb05180f252ba71dcf50475
Massimo Paladin <massimo.paladin@gmail.com>
2026-03-24 10:40:27 +0100

    Update GitHub Actions versions and Sonar scan args (#7)
    * Update GitHub Actions versions and Sonar scan args
    
    * Update Sonar Scanner CLI to 8.0.1.6346
    
    * Use macosx-aarch64 Sonar Scanner download URL
    
    * Use full-depth checkout in workflow
    
    * Revert "Use full-depth checkout in workflow"
    
    This reverts commit ed6ec8798fee45d734280892937ef5cd5fd5ac3c.

--- build-analyze.sh
+++ build-analyze.sh
@@ -6,7 +6,7 @@ git fetch --unshallow
 
 SONAR_HOST_URL=https://sonarcloud.io
 #SONAR_TOKEN= # Access token coming from SonarCloud projet creation page. In this example, it is defined in the environement through a Github secret.
-export SONAR_SCANNER_VERSION="5.0.1.3006" # Find the latest version in the "Mac OS" link on this page:
+export SONAR_SCANNER_VERSION="8.0.1.6346" # Find the latest version in the "Mac OS" link on this page:
                                           # https://docs.sonarcloud.io/advanced-setup/ci-based-analysis/sonarscanner-cli/
 export BUILD_WRAPPER_OUT_DIR="build_wrapper_output_directory" # Directory where build-wrapper output will be placed
 
@@ -18,9 +18,9 @@ unzip -o $HOME/.sonar/build-wrapper-macosx-x86.zip -d $HOME/.sonar/
 export PATH=$HOME/.sonar/build-wrapper-macosx-x86:$PATH
 
 # Download sonar-scanner
-curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx.zip 
+curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx-aarch64.zip 
 unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-export PATH=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx/bin:$PATH
+export PATH=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx-aarch64/bin:$PATH
 
 # Setup the build system
 xcodebuild -project macos-xcode.xcodeproj clean
